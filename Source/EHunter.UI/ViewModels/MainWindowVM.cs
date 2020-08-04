52ec5a9a5b6df860e50603a30f22a1a34d0e81da
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-08-04 23:11:23 +0800

    Basic tab viewmodels.

--- Source/EHunter.UI/ViewModels/MainWindowVM.cs
+++ Source/EHunter.UI/ViewModels/MainWindowVM.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.UI.Xaml.Controls;
 
 namespace EHunter.UI.ViewModels
 {
     internal class MainWindowVM : ObservableObject
     {
+        public ObservableCollection<TabRootVM> Tabs { get; } = new ObservableCollection<TabRootVM>
+        {
+            new TabRootVM()
+        };
 
+        public void AddNewTab() => Tabs.Add(new TabRootVM());
+
+        public void CloseTab(TabView _, TabViewTabCloseRequestedEventArgs args)
+            => Tabs.Remove((TabRootVM)args.Item);
     }
 }
---
+++ Source/EHunter.UI/ViewModels/TabRootVM.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace EHunter.UI.ViewModels
+{
+    internal class TabRootVM : ObservableObject
+    {
+        private string _header = "New Tab";
+        public string Header
+        {
+            get => _header;
+            set => SetProperty(ref _header, value);
+        }
+    }
+}
