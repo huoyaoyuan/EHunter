858acc3e40493642407e661f5b2e6c9bc6ebaa92
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-09-14 17:46:02 +0800

    Basic content for recent updated page.

--- Source/EHunter.Provider.Pixiv/Models/PixivRecentVM.cs
+++ Source/EHunter.Provider.Pixiv/Models/PixivRecentVM.cs
@@ -1,9 +1,15 @@
-﻿using Meowtrix.PixivApi;
+﻿using System.Collections.ObjectModel;
+using System.Threading;
+using System.Threading.Tasks;
+using Meowtrix.PixivApi;
+using Meowtrix.PixivApi.Models;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
 
 namespace EHunter.Provider.Pixiv.Models
 {
+#pragma warning disable CA1001 // 具有可释放字段的类型应该是可释放的
     public class PixivRecentVM : ObservableObject
+#pragma warning restore CA1001 // 具有可释放字段的类型应该是可释放的
     {
         private readonly PixivClient _client;
 
@@ -19,15 +25,41 @@ namespace EHunter.Provider.Pixiv.Models
             }
         }
 
-        public void Refresh()
+        public async void Refresh()
         {
+            if (_lastCts != null)
+            {
+                _lastCts.Cancel();
+                _lastCts.Dispose();
+                _lastCts = null;
+            }
+
             if (_client.IsLogin)
             {
                 NotLogin = false;
+                var illusts = Illusts = new ObservableCollection<IllustPage>();
+                var cts = _lastCts = new CancellationTokenSource();
+                try
+                {
+                    var source = _client.GetMyFollowingIllustsAsync();
+                    source = SelectedModeIndex switch
+                    {
+                        1 => source.AllAge(),
+                        2 => source.R18(),
+                        _ => source
+                    };
+
+                    await foreach (var i in source.WithCancellation(cts.Token).ConfigureAwait(true))
+                        illusts.Add(i.Pages[0]);
+                }
+                catch
+                {
+                }
             }
             else
             {
                 NotLogin = true;
+                Illusts = null;
             }
         }
 
@@ -36,6 +68,26 @@ namespace EHunter.Provider.Pixiv.Models
         {
             get => _notLogin;
             private set => SetProperty(ref _notLogin, value);
+        }
+
+        private int _selectedModeIndex = 1;
+        public int SelectedModeIndex
+        {
+            get => _selectedModeIndex;
+            set
+            {
+                if (SetProperty(ref _selectedModeIndex, value))
+                    Refresh();
+            }
+        }
+
+        private CancellationTokenSource? _lastCts;
+
+        private ObservableCollection<IllustPage>? _illusts;
+        public ObservableCollection<IllustPage>? Illusts
+        {
+            get => _illusts;
+            private set => SetProperty(ref _illusts, value);
         }
     }
 }
