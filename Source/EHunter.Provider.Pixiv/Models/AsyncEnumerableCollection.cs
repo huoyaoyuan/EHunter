0b2d88345efd6c9de432513bb8e23660f1cb76aa
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-09-22 00:19:35 +0800

    Add a collection for async enumerable and incremental loading.

---
+++ Source/EHunter.Provider.Pixiv/Models/AsyncEnumerableCollection.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml.Data;
+using Windows.Foundation;
+
+namespace EHunter.Provider.Pixiv.Models
+{
+    public class AsyncEnumerableCollection<T> :
+        ObservableCollection<T>,
+        ISupportIncrementalLoading
+    {
+        private readonly ConfiguredCancelableAsyncEnumerable<T>.Enumerator _enumerator;
+
+        public AsyncEnumerableCollection(
+            IAsyncEnumerable<T> valueEnumerable,
+            CancellationToken cancellation = default)
+        {
+            _enumerator = valueEnumerable
+                .ConfigureAwait(true)
+                .WithCancellation(cancellation)
+                .GetAsyncEnumerator();
+        }
+
+        public IAsyncOperation<LoadMoreItemsResult> LoadMoreItemsAsync(uint count)
+            => LoadMoreItemsAsyncCore(count).AsAsyncOperation();
+
+        private async Task<LoadMoreItemsResult> LoadMoreItemsAsyncCore(uint count)
+        {
+            uint loaded = 0;
+
+            while (loaded < count)
+            {
+                try
+                {
+                    if (await _enumerator.MoveNextAsync())
+                    {
+                        Add(_enumerator.Current);
+                        loaded++;
+                    }
+                    else
+                    {
+                        HasMoreItems = false;
+                        await _enumerator.DisposeAsync();
+                        break;
+                    }
+                }
+                catch
+                {
+                    HasMoreItems = false;
+                    break;
+                }
+            }
+
+            return new LoadMoreItemsResult(loaded);
+        }
+
+        private bool _hasMoreItems = true;
+        public bool HasMoreItems
+        {
+            get => _hasMoreItems;
+            private set
+            {
+                if (_hasMoreItems != value)
+                {
+                    _hasMoreItems = value;
+                    OnPropertyChanged(new PropertyChangedEventArgs(nameof(HasMoreItems)));
+                }
+            }
+        }
+    }
+}
