13df5e0cc50f7cad09e0d8cd6d5ce30d30df2123
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-09-23 17:29:25 +0800

    Use a VM for each user.

---
+++ Source/EHunter.Provider.Pixiv/ViewModels/UserSearchVM.cs
@@ -0,0 +1,17 @@
+﻿using Meowtrix.PixivApi.Models;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+#nullable enable
+
+namespace EHunter.Provider.Pixiv.ViewModels
+{
+    public class UserSearchVM : ObservableObject
+    {
+        private UserInfo? _userInfo;
+        public UserInfo? UserInfo
+        {
+            get => _userInfo;
+            set => SetProperty(ref _userInfo, value);
+        }
+    }
+}
--- Source/EHunter.Provider.Pixiv/Views/UsersView.xaml.cs
+++ Source/EHunter.Provider.Pixiv/Views/UsersView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using EHunter.Provider.Pixiv.Messages;
+﻿using System.Linq;
+using EHunter.Provider.Pixiv.Messages;
+using EHunter.Provider.Pixiv.ViewModels;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
 
@@ -18,7 +20,26 @@ namespace EHunter.Provider.Pixiv.Views
         {
             if (e.Parameter is NavigateToUserMessage m)
             {
-                tabView.TabItems.Add(m.User);
+                // TODO: SelectedItem doesn't work. SelectedIndex neither. Identify why in next WinUI preview.
+
+                var vm = tabView.TabItems.Cast<UserSearchVM>().FirstOrDefault(x => x.UserInfo == m.User);
+                if (vm is null)
+                {
+                    tabView.TabItems.Add(vm = new UserSearchVM { UserInfo = m.User });
+                }
+
+                tabView.SelectedItem = vm;
+
+                //var tabItems = tabView.TabItems;
+                //int index;
+                //for (index = 0; index < tabItems.Count; index++)
+                //    if (tabItems[index] is UserSearchVM vm && vm.UserInfo == m.User)
+                //        break;
+
+                //if (index == tabItems.Count)
+                //    tabItems.Add(new UserSearchVM { UserInfo = m.User });
+
+                //tabView.SelectedIndex = index;
             }
         }
     }
