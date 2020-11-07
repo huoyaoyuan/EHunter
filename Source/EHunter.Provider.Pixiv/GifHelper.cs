6654be0a6c32510ad0c5399a108cd96201c6aa23
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-11-07 19:01:45 +0800

    Add GifHelper using ImageSharp.

---
+++ Source/EHunter.Provider.Pixiv/GifHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Meowtrix.PixivApi.Models;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace EHunter.Provider.Pixiv
+{
+    public static class GifHelper
+    {
+        public static async ValueTask<byte[]> ComposeGifAsync(Illust illust)
+        {
+            if (!illust.IsAnimated)
+                throw new InvalidOperationException("The illustration must be animated.");
+
+            var details = await illust.GetAnimatedDetailAsync().ConfigureAwait(false);
+            Image<Rgba32>? image = null;
+
+            foreach (var (stream, frametime) in await details.ExtractFramesAsync().ConfigureAwait(false))
+            {
+                using (stream)
+                {
+                    var frame = await Image.LoadAsync(stream).ConfigureAwait(false);
+                    image ??= new Image<Rgba32>(frame.Width, frame.Height);
+                    image.Frames.AddFrame(frame.Frames[0]);
+                }
+            }
+
+            if (image is null)
+                throw new InvalidOperationException("No frame added to the animated illust.");
+
+            image.Frames.RemoveFrame(0);
+
+            using var mms = new MemoryStream();
+            await image.SaveAsGifAsync(mms).ConfigureAwait(false);
+
+            byte[] array = new byte[mms.Length];
+            mms.Seek(0, SeekOrigin.Begin);
+            mms.Read(array);
+            return array;
+        }
+    }
+}
