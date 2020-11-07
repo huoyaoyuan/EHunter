cd040a620d8baf580b2f255b1b4543c942fa260e
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-11-07 20:29:40 +0800

    Add animated illust to IllustFlip.

---
+++ Source/EHunter.Provider.Pixiv/Controls/IllustFlipTemplateSelector.cs
@@ -0,0 +1,21 @@
+﻿using Meowtrix.PixivApi.Models;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+#nullable enable
+
+namespace EHunter.Provider.Pixiv.Controls
+{
+    public sealed class IllustFlipTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate? AnimatedTemplate { get; set; }
+        public DataTemplate? NonAnimatedTemplate { get; set; }
+
+        protected override DataTemplate? SelectTemplateCore(object item, DependencyObject container)
+            => SelectTemplateCore(item);
+        protected override DataTemplate? SelectTemplateCore(object item)
+            => item is Illust { IsAnimated: true }
+            ? AnimatedTemplate
+            : NonAnimatedTemplate;
+    }
+}
