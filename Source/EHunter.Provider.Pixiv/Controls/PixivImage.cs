50d74487f9874fcedc03f0fe0b53b2902a99f9be
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-11-20 15:31:48 +0800

    Move namespace of image request.

--- Source/EHunter.Provider.Pixiv/Controls/PixivImage.cs
+++ Source/EHunter.Provider.Pixiv/Controls/PixivImage.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using EHunter.Controls;
-using EHunter.Provider.Pixiv.Services.ImageCaching;
+using EHunter.Services.ImageCaching;
 using Meowtrix.PixivApi.Models;
 
 #nullable enable
@@ -22,5 +23,24 @@ namespace EHunter.Provider.Pixiv.Controls
                 }
             }
         }
+    }
+
+    internal sealed class PixivIllustRequest : ImageRequest
+    {
+        private readonly ImageInfo _imageInfo;
+
+        public PixivIllustRequest(ImageInfo imageInfo) => _imageInfo = imageInfo;
+
+        private record CacheKey(string UriString);
+        public override object MemoryCacheKey => new CacheKey(_imageInfo.Uri.OriginalString);
+
+        public override async Task<byte[]> GetImageAsync()
+        {
+            using var response = await _imageInfo.RequestAsync().ConfigureAwait(false);
+            return await response.Content.ReadAsByteArrayAsync().ConfigureAwait(false);
+        }
+
+        public static PixivIllustRequest? TryCreate(ImageInfo? imageInfo)
+            => imageInfo is { } info ? new(info) : null;
     }
 }
