f92d5544d1377a40e5d05c27897f174b935f1986
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-12-22 22:07:07 +0800

    Design time DbContext factory.

---
+++ Source/EHunter.Data/EHunterDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace EHunter.Data
+{
+    internal class EHunterDbContextFactory : IDesignTimeDbContextFactory<EHunterDbContext>
+    {
+        public EHunterDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<EHunterDbContext>();
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Integrated Security=true",
+                sql => sql.UseHierarchyId());
+            return new EHunterDbContext(optionsBuilder.Options);
+        }
+    }
+}
