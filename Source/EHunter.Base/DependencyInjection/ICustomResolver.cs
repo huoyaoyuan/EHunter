5edc4c5b0ba8f88cdaf60cc47ffb13e8b3cefaab
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-12-27 19:12:28 +0800

    Custom service resolver interface.

---
+++ Source/EHunter.Base/DependencyInjection/ICustomResolver.cs
@@ -0,0 +1,24 @@
+﻿namespace EHunter.DependencyInjection
+{
+    public interface ICustomResolver<out T> where T : class
+    {
+        T Resolve();
+    }
+
+    public static class ResolverExtensions
+    {
+        private class WrappedResolver<T> : ICustomResolver<T>
+            where T : class
+        {
+            private readonly T _service;
+
+            public WrappedResolver(T service) => _service = service;
+
+            public T Resolve() => _service;
+        }
+
+        public static ICustomResolver<T> AsServiceResolver<T>(this T service)
+            where T : class
+            => new WrappedResolver<T>(service);
+    }
+}
