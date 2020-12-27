7828e732683f7b1d650f813f4371100ca8e7cae2
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-12-27 19:33:43 +0800

    Extension for registering singleton as multiple services.

--- Source/EHunter.Base/DependencyInjection/ICustomResolver.cs
+++ Source/EHunter.Base/DependencyInjection/ICustomResolver.cs
@@ -4,21 +4,4 @@
     {
         T Resolve();
     }
-
-    public static class ResolverExtensions
-    {
-        private class WrappedResolver<T> : ICustomResolver<T>
-            where T : class
-        {
-            private readonly T _service;
-
-            public WrappedResolver(T service) => _service = service;
-
-            public T Resolve() => _service;
-        }
-
-        public static ICustomResolver<T> AsServiceResolver<T>(this T service)
-            where T : class
-            => new WrappedResolver<T>(service);
-    }
 }
