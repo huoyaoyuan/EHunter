041987b22812f9b71ec0c1376153c86668984450
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-12-28 00:17:08 +0800

    Pixiv client service as custom resolver.

--- Source/EHunter.Provider.Pixiv/PixivIoc.cs
+++ Source/EHunter.Provider.Pixiv/PixivIoc.cs
@@ -1,7 +1,9 @@
-﻿using EHunter.Provider.Pixiv.Models;
+﻿using EHunter.DependencyInjection;
+using EHunter.Provider.Pixiv.Models;
 using EHunter.Provider.Pixiv.Services;
 using EHunter.Provider.Pixiv.ViewModels;
 using EHunter.Services.ImageCaching;
+using Meowtrix.PixivApi;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace EHunter.Provider.Pixiv
@@ -13,6 +15,8 @@ namespace EHunter.Provider.Pixiv
             .AddSingleton<PixivSettings>()
             .AddSingleton<PixivSettingStore>()
             .AddSingleton<PixivSetting2>()
+            .AddSingleton<PixivClientService>()
+            .AddConversion<ICustomResolver<PixivClient>, PixivClientService>()
             .AddSingleton<PixivRecentVM>()
             .AddSingleton<UserVMFactory>()
             .AddSingleton<OpenedIllustsVM>()
