a774daa92dc6240e9585046af0eb6e973c47f719
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-12-28 17:35:33 +0800

    New view model for pixiv login page.

---
+++ Source/EHunter.Provider.Pixiv/ViewModels/PixivLoginPageVM.cs
@@ -0,0 +1,128 @@
+﻿using System.Threading.Tasks;
+using EHunter.Provider.Pixiv.Models;
+using EHunter.Provider.Pixiv.Services;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+#nullable enable
+
+namespace EHunter.Provider.Pixiv.ViewModels
+{
+    public class PixivLoginPageVM : ObservableObject
+    {
+        private readonly PixivSettingStore _settingStore;
+        private readonly PixivSetting2 _setting;
+        private readonly PixivClientService _clientService;
+
+        public PixivLoginPageVM(
+            PixivSettingStore settingStore,
+            PixivSetting2 setting,
+            PixivClientService clientService,
+            DatabaseAccessor databaseAccessor)
+        {
+            _settingStore = settingStore;
+            _setting = setting;
+            _clientService = clientService;
+
+            _useProxy = setting.UseProxy;
+
+            string? savedToken = settingStore.RefreshToken;
+            if (!string.IsNullOrEmpty(savedToken))
+            {
+                RefreshToken = savedToken;
+                LoginWithToken();
+            }
+
+            InitDatabase();
+            async void InitDatabase()
+            {
+                var factory = await databaseAccessor.FactoryTask.ConfigureAwait(false);
+                DatabaseInitState = factory is not null;
+                CheckInitialize();
+            }
+        }
+
+        private string _username = string.Empty;
+        public string Username
+        {
+            get => _username;
+            set => SetProperty(ref _username, value);
+        }
+
+        private string _password = string.Empty;
+        public string Password
+        {
+            get => _password;
+            set => SetProperty(ref _password, value);
+        }
+
+        private string _refreshToken = string.Empty;
+        public string RefreshToken
+        {
+            get => _refreshToken;
+            set => SetProperty(ref _refreshToken, value);
+        }
+
+        private bool _isLoggingIn;
+        public bool IsLoggingIn
+        {
+            get => _isLoggingIn;
+            private set => SetProperty(ref _isLoggingIn, value);
+        }
+
+        private bool _isLoggedin;
+        public bool IsLoggedin
+        {
+            get => _isLoggedin;
+            private set => SetProperty(ref _isLoggedin, value);
+        }
+
+        private bool _useProxy;
+        public bool UseProxy
+        {
+            get => _useProxy;
+            set
+            {
+                if (SetProperty(ref _useProxy, value))
+                    _setting.SetUseProxy(value);
+            }
+        }
+
+        public void LoginWithPassword() => PerformLogin(_clientService.LoginAsync(Username, Password));
+
+        public void LoginWithToken() => PerformLogin(_clientService.LoginAsync(RefreshToken));
+
+        private async void PerformLogin(Task<string> loginTask)
+        {
+            IsLoggingIn = true;
+
+            try
+            {
+                string refreshToken = await loginTask.ConfigureAwait(true);
+                _settingStore.RefreshToken = refreshToken;
+                IsLoggedin = true;
+            }
+            catch
+            {
+                // login failed message
+            }
+
+            IsLoggingIn = false;
+            CheckInitialize();
+        }
+
+        private bool? _databaseInitState;
+        public bool? DatabaseInitState
+        {
+            get => _databaseInitState;
+            private set => SetProperty(ref _databaseInitState, value);
+        }
+
+        private void CheckInitialize()
+        {
+            if (IsLoggedin && DatabaseInitState != null)
+            {
+                // jump to next page
+            }
+        }
+    }
+}
