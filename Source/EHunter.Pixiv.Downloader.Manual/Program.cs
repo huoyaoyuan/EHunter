528a4703ddef7ee41f1656e5e36408a248765006
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-12-29 22:35:45 +0800

    Capture deleted illusts of Pixiv.

--- Source/EHunter.Pixiv.Downloader.Manual/Program.cs
+++ Source/EHunter.Pixiv.Downloader.Manual/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Reactive.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using EHunter.Data;
 using EHunter.Data.Pixiv;
@@ -69,7 +72,7 @@ var downloadService = new DownloaderService(
 Console.Write("Seed folder:");
 var folder = new DirectoryInfo(Console.ReadLine()!);
 
-var skipped = new List<int>();
+var skipped = new List<(int id, FileInfo file)>();
 
 foreach (var f in folder.EnumerateFiles("*", SearchOption.AllDirectories))
 {
@@ -93,7 +96,7 @@ foreach (var f in folder.EnumerateFiles("*", SearchOption.AllDirectories))
     catch (Exception ex)
     {
         Console.WriteLine($"Unable to get metadata for {id}. Skipping. ({ex.Message})");
-        skipped.Add(id);
+        skipped.Add((id, f));
         continue;
     }
 
@@ -121,7 +124,66 @@ if (skipped.Count > 0)
     var oldColor = Console.ForegroundColor;
     Console.ForegroundColor = ConsoleColor.Yellow;
     Console.WriteLine("Skipped images:");
-    foreach (int i in skipped)
+    foreach (int i in skipped.Select(x => x.id).Distinct())
         Console.WriteLine(i);
     Console.ForegroundColor = oldColor;
+
+    Console.Write("Add skipped to missing?:(y/n)");
+    if (Console.ReadLine()!.StartsWith("y", StringComparison.OrdinalIgnoreCase))
+    {
+        var regex = new Regex(@"(\d+)_p(\d+).*", RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
+
+        foreach (var g in skipped.GroupBy(x => x.id))
+        {
+            var orders = g.Select(x =>
+            {
+                var match = regex.Match(x.file.Name);
+                string n = match.Groups[2].Value;
+                return (order: int.Parse(n, NumberFormatInfo.InvariantInfo), x.file);
+            }).ToArray();
+            using var eContext = eFactory.CreateDbContext();
+            var post = new Post
+            {
+                Provider = "Pixiv:Illust",
+                Identifier = g.Key,
+                Url = new Uri($"https://www.pixiv.net/artworks/{g.Key}")
+            };
+
+            foreach (var o in orders)
+            {
+                Directory.CreateDirectory(Path.Combine(storageSetting.StorageRoot!.FullName, "Pixiv", "Deleted"));
+
+                string dest = Path.Combine("Pixiv", "Deleted", o.file.Name);
+                o.file.CopyTo(Path.Combine(storageSetting.StorageRoot!.FullName, dest), overwrite: true);
+
+                post.Images.Add(new ImageEntry(ImageType.Static, dest)
+                {
+                    PostOrderId = o.order
+                });
+                post.FavoritedTime = o.file.CreationTimeUtc.ToLocalTime();
+            }
+
+            if (post.Images.Count == 1)
+            {
+                post.Images[0].Tags.Add(new ImageTag("Pixiv:Meta", "Deleted"));
+            }
+            else
+            {
+                var gallery = new PostGallery
+                {
+                    Post = post,
+                    Tags =
+                    {
+                        new GalleryTag("Pixiv:Meta", "Deleted")
+                    }
+                };
+                eContext.PostGalleries.Add(gallery);
+            }
+
+            eContext.Posts.Add(post);
+
+            await eContext.SaveChangesAsync();
+            Console.WriteLine($"Added deleted {g.Key}.");
+        }
+    }
 }
