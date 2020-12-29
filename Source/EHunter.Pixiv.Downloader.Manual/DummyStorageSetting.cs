6de1a417468c45592e25f7aedcac8155c07b9e41
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-12-29 11:37:35 +0800

    Manual download test.

---
+++ Source/EHunter.Pixiv.Downloader.Manual/DummyStorageSetting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using EHunter.Settings;
+
+namespace EHunter.Pixiv.Downloader.Manual
+{
+    internal class DummyStorageSetting : IStorageSetting, IObservable<DirectoryInfo?>
+    {
+        public DummyStorageSetting(string storageRoot) => StorageRoot = new DirectoryInfo(storageRoot);
+
+        public DirectoryInfo? StorageRoot { get; }
+        public IObservable<DirectoryInfo?> StorageChanged => this;
+
+        public IDisposable Subscribe(IObserver<DirectoryInfo?> observer) => new DummyDisposable();
+
+        private class DummyDisposable : IDisposable
+        {
+            public void Dispose() { }
+        }
+    }
+}
---
+++ Source/EHunter.Pixiv.Downloader.Manual/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using EHunter.Data;
+using EHunter.Data.Pixiv;
+using EHunter.DependencyInjection;
+using EHunter.Pixiv.Downloader.Manual;
+using EHunter.Provider.Pixiv.Services.Download;
+using Meowtrix.PixivApi;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+#pragma warning disable CA2007
+
+Console.Write("Storage:");
+var storageSetting = new DummyStorageSetting(Console.ReadLine()!);
+
+Console.Write("Proxy:");
+string? proxy = Console.ReadLine();
+var webProxy = string.IsNullOrEmpty(proxy) ? null : new WebProxy(proxy);
+
+Console.Write("Pixiv refresh token:");
+var pixivClient = new PixivClient(webProxy);
+await pixivClient.LoginAsync(Console.ReadLine()!);
+
+Console.Write("LocalDB database name:");
+string connectionString = @"Server=(localdb)\MSSQLLocalDB;Integrated Security=true;Database=" + Console.ReadLine();
+
+var sp = new ServiceCollection()
+    .AddPooledDbContextFactory<EHunterDbContext>(o => o.UseSqlServer(connectionString))
+    .AddPooledDbContextFactory<PixivDbContext>(o => o.UseSqlServer(connectionString))
+    .BuildServiceProvider();
+
+var eFactory = sp.GetRequiredService<IDbContextFactory<EHunterDbContext>>();
+var pFactory = sp.GetRequiredService<IDbContextFactory<PixivDbContext>>();
+
+using (var eContext = eFactory.CreateDbContext())
+    await eContext.Database.MigrateAsync();
+using (var pContext = pFactory.CreateDbContext())
+    await pContext.Database.MigrateAsync();
+
+var downloadService = new DownloaderService(
+    pixivClient.AsServiceResolver(),
+    eFactory.AsServiceResolver(),
+    pFactory.AsServiceResolver(),
+    storageSetting);
+
+Console.Write("Download by id?:");
+if (int.TryParse(Console.ReadLine(), out int id))
+{
+    var task = await downloadService.CreateDownloadTaskAsync(id);
+    var tcs = new TaskCompletionSource();
+    task.Progress
+        .Subscribe(
+            p => Console.WriteLine($"Progress: {p:P}"),
+            ex => tcs.SetException(ex),
+            () => tcs.SetResult());
+    task.Start();
+    await tcs.Task;
+    return;
+}
