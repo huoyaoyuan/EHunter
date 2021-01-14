00647c5b6944d9d8cb096ebff0ef6096c09228fb
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-01-15 01:20:14 +0800

    Show preview illusts on following users.

---
+++ Source/Pixiv/UI/Controls/Helpers.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Meowtrix.PixivApi.Models;
+
+namespace EHunter.Pixiv.Controls
+{
+    internal static class Helpers
+    {
+        public static ImageInfo FirstPageMedium(IReadOnlyList<IllustPage> pages)
+            => pages[0].Medium;
+    }
+}
