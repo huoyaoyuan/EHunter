3b2cf524136b4dd59e342105a8a3634e3d3992e0
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-01-16 19:28:14 +0800

    Navigation to ranking page.

---
+++ Source/Pixiv/UI/Views/Ranking/RankingPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace EHunter.Pixiv.Views.Ranking
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class RankingPage : Page
+    {
+        public RankingPage() => InitializeComponent();
+    }
+}
