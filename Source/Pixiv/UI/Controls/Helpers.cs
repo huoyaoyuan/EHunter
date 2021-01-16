cde939b83c11b48486851f13a77a9fb25331a6bb
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-01-16 17:37:08 +0800

    Loading resource from MRMCore in code.

--- Source/Pixiv/UI/Controls/Helpers.cs
+++ Source/Pixiv/UI/Controls/Helpers.cs
@@ -1,11 +1,31 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Meowtrix.PixivApi;
 using Meowtrix.PixivApi.Models;
+using Microsoft.ApplicationModel.Resources;
+
+#nullable enable
 
 namespace EHunter.Pixiv.Controls
 {
     internal static class Helpers
     {
+        private static readonly Lazy<ResourceLoader> s_resourceLoader = new(() =>
+        {
+            string? name = typeof(Helpers).Assembly.GetName().Name;
+            return new ResourceLoader(name + ".pri", name);
+        });
+
         public static ImageInfo FirstPageMedium(IReadOnlyList<IllustPage> pages)
             => pages[0].Medium;
+
+        public static string LoadFromResources(string key)
+            => s_resourceLoader.Value.GetString("Resources/" + key);
+
+        public static string GetIllustRankingMode(int intRankingMode)
+        {
+            var mode = (IllustRankingMode)intRankingMode;
+            return LoadFromResources($"IllustRankingMode_{mode}");
+        }
     }
 }
