cb306e3bcafaa36003e7ea088ddd8dd72297ee4d
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-01-18 16:58:15 +0800

    Navigation to search user.

--- Source/Pixiv/UI/Views/Search/UserSearchPage.xaml.cs
+++ Source/Pixiv/UI/Views/Search/UserSearchPage.xaml.cs
@@ -3,7 +3,7 @@
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace EHunter.Pixiv.Views
+namespace EHunter.Pixiv.Views.Search
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
