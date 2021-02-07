33e9aa2f842464e2a3bffe4065f56b20a3aa8063
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-02-07 23:11:49 +0800

    Adapter from MEF back to MSDI.

---
+++ Source/App/WinUI/Composition/MEFServiceProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Composition.Hosting;
+
+#nullable enable
+
+namespace EHunter.UI.Composition
+{
+    internal class MEFServiceProvider : IServiceProvider
+    {
+        private readonly CompositionHost _host;
+
+        public MEFServiceProvider(CompositionHost host) => _host = host;
+
+        public object? GetService(Type serviceType)
+            => _host.TryGetExport(serviceType, out object obj)
+            ? obj : null;
+    }
+}
