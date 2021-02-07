8740c8bb4e1a306c04cebcef9a2732a5634c5665
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-02-07 22:01:40 +0800

    Add adapter from MSDI to MEF.

---
+++ Source/App/WinUI/Composition/MEFDependencyInjectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Composition.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+#nullable enable
+
+namespace EHunter.UI.Composition
+{
+    public static class MEFDependencyInjectionExtensions
+    {
+        public static ContainerConfiguration WithServiceCollection(
+            this ContainerConfiguration container,
+            IServiceCollection serviceCollection)
+            => container.WithProvider(new ServiceExportProvider(serviceCollection));
+    }
+}
