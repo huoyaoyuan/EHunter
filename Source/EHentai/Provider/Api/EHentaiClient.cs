18286bc09cf048e3a430a87b35c69f08e3df6921
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-03-06 00:56:33 +0800

    EHentai login and restore.

--- Source/EHentai/Provider/Api/EHentaiClient.cs
+++ Source/EHentai/Provider/Api/EHentaiClient.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace EHunter.EHentai.Api
 {
@@ -32,5 +34,39 @@ namespace EHunter.EHentai.Api
         }
 
         public void Dispose() => _httpClient.Dispose();
+
+        public async Task LoginAsync(string username, string password)
+        {
+            using var request = new HttpRequestMessage(HttpMethod.Post, "https://forums.e-hentai.org/index.php?act=Login&CODE=01")
+            {
+                Content = new FormUrlEncodedContent(new Dictionary<string, string>
+                {
+                    { "UserName", username },
+                    { "PassWord", password },
+                    { "CookieDate", "1" },
+                }!)
+            };
+
+            await _httpClient.SendAsync(request).ConfigureAwait(false);
+        }
+
+        public (string memberId, string passHash) SaveLogin()
+        {
+            var cookies = _cookies.GetCookies(new Uri("https://e-hentai.org"));
+            return (cookies["ipb_member_id"]?.Value ?? throw new InvalidOperationException("No login cookies"),
+                cookies["ipb_pass_hash"]?.Value ?? throw new InvalidOperationException("No login cookies"));
+        }
+
+        public void RestoreLogin(string memberId, string passHash)
+        {
+            _cookies.Add(new Cookie("ipb_member_id", memberId, null, "e-hentai.org"));
+            _cookies.Add(new Cookie("ipb_pass_hash", passHash, null, "e-hentai.org"));
+
+            _cookies.Add(new Cookie("ipb_member_id", memberId, null, "forums.e-hentai.org"));
+            _cookies.Add(new Cookie("ipb_pass_hash", passHash, null, "forums.e-hentai.org"));
+
+            _cookies.Add(new Cookie("ipb_member_id", memberId, null, "exhentai.org"));
+            _cookies.Add(new Cookie("ipb_pass_hash", passHash, null, "exhentai.org"));
+        }
     }
 }
