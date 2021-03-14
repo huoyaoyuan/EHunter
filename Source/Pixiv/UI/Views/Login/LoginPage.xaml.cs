d6dc7946f11703535b60cf339229fd8651d0bcb5
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-03-15 00:22:20 +0800

    Move LoginPage into namespace.

--- Source/Pixiv/UI/Views/Login/LoginPage.xaml.cs
+++ Source/Pixiv/UI/Views/Login/LoginPage.xaml.cs
@@ -2,7 +2,6 @@
 using System.Threading.Tasks;
 using EHunter.Pixiv.Messages;
 using EHunter.Pixiv.ViewModels;
-using EHunter.Pixiv.Views.Login;
 using Microsoft.Toolkit.Mvvm.DependencyInjection;
 using Microsoft.Toolkit.Mvvm.Messaging;
 using Microsoft.UI.Xaml;
@@ -12,7 +11,7 @@ using Microsoft.UI.Xaml.Controls.Primitives;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace EHunter.Pixiv.Views
+namespace EHunter.Pixiv.Views.Login
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
