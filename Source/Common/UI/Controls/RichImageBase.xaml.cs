fae36115a6b275ed240660e01887b2cea3554d93
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-03-18 14:55:14 +0800

    Remove WinRT stream workaround for image.

--- Source/Common/UI/Controls/RichImageBase.xaml.cs
+++ Source/Common/UI/Controls/RichImageBase.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using EHunter.Services.ImageCaching;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
 using Microsoft.Toolkit.Mvvm.DependencyInjection;
@@ -70,15 +71,8 @@ namespace EHunter.Controls
 
                 var source = new BitmapImage();
 
-                // https://github.com/microsoft/CsWinRT/issues/682
-                // only WinRT stream with SetSource can avoid random exception
                 using (var stream = _imageEntry.GetStream())
-                using (var winrtStream = new InMemoryRandomAccessStream())
-                {
-                    stream.CopyTo(winrtStream.AsStream());
-                    winrtStream.Seek(0);
-                    source.SetSource(winrtStream);
-                }
+                    await source.SetSourceAsync(stream.AsRandomAccessStream());
 
                 Source = source;
                 IsLoading = false;
