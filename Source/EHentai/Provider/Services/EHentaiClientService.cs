f119ddf258ee9f388450a7d3cf1b5358c8836a04
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-04-19 16:32:19 +0800

    EHentai client service.

---
+++ Source/EHentai/Provider/Services/EHentaiClientService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Composition;
+using System.Net.Http;
+using System.Reactive.Linq;
+using EHunter.DependencyInjection;
+using EHunter.EHentai.Api;
+using EHunter.EHentai.Settings;
+using EHunter.Settings;
+
+namespace EHunter.EHentai.Services
+{
+    [Export, Export(typeof(ICustomResolver<EHentaiClient>)), Shared]
+    public sealed class EHentaiClientService : ICustomResolver<EHentaiClient>, IDisposable
+    {
+        private readonly IDisposable _subscribeDisposable;
+        private readonly EHentaiClient _client = new();
+
+        [ImportingConstructor]
+        public EHentaiClientService(IProxySetting proxySetting, EHentaiSetting eHentaiSetting)
+        {
+            _subscribeDisposable = proxySetting.Proxy
+                .CombineLatest(eHentaiSetting.ConnectionMode,
+                    (proxy, mode) => mode switch
+                    {
+                        EHentaiConnectionMode.ApplicationProxy => proxy,
+                        EHentaiConnectionMode.SystemProxy => HttpClient.DefaultProxy,
+                        EHentaiConnectionMode.NoProxy => null,
+                        _ => throw new InvalidOperationException($"Unknown enum value {mode}.")
+                    })
+                .Subscribe(proxy => _client.SetProxy(proxy));
+        }
+
+        public void Dispose()
+        {
+            _subscribeDisposable.Dispose();
+            _client.Dispose();
+        }
+
+        public EHentaiClient Resolve() => _client;
+    }
+}
