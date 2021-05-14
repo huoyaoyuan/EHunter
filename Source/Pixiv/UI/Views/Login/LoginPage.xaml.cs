2c5a1d43eb057b0bb161820e74c6f71bd2af83f3
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-05-14 16:39:25 +0800

    Remove Flyout and message for login failed.

--- Source/Pixiv/UI/Views/Login/LoginPage.xaml.cs
+++ Source/Pixiv/UI/Views/Login/LoginPage.xaml.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.DependencyInjection;
-using CommunityToolkit.Mvvm.Messaging;
-using EHunter.Pixiv.Messages;
 using EHunter.Pixiv.ViewModels;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -17,24 +14,12 @@ namespace EHunter.Pixiv.Views.Login
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
 #pragma warning disable CA1708
-    public sealed partial class LoginPage : Page, IRecipient<LoginFailedMessage>
+    public sealed partial class LoginPage : Page
 #pragma warning restore CA1708
     {
         private readonly LoginPageVM _vm = Ioc.Default.GetRequiredService<LoginPageVM>();
 
-        public LoginPage()
-        {
-            InitializeComponent();
-
-            Loaded += (s, e) => WeakReferenceMessenger.Default.Register(this);
-            Unloaded += (s, e) => WeakReferenceMessenger.Default.UnregisterAll(this);
-        }
-
-        void IRecipient<LoginFailedMessage>.Receive(LoginFailedMessage message)
-        {
-            loginFailedMessage.Text = message.Exception.Message;
-            FlyoutBase.ShowAttachedFlyout(loginPanel);
-        }
+        public LoginPage() => InitializeComponent();
 
         private void DoPasswordLogin(object sender, RoutedEventArgs e)
         {
