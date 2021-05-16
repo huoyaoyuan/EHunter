88c8c3c5572b78116f98641bfaa5f26f4cf4419d
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-05-16 17:03:48 +0800

    Set navigation type as attatched property.

---
+++ Source/Common/UI/Controls/NavigationHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+#nullable enable
+
+namespace EHunter.Controls
+{
+    public static class NavigationHelper
+    {
+        public static readonly DependencyProperty NavigationTypeProperty
+            = DependencyProperty.RegisterAttached("NavigationType", typeof(Type), typeof(NavigationViewItem),
+                new PropertyMetadata(null));
+        public static Type GetNavigationType(NavigationViewItem dependencyObject)
+            => (Type)dependencyObject.GetValue(NavigationTypeProperty);
+        public static void SetNavigationType(NavigationViewItem dependencyObject, Type value)
+            => dependencyObject.SetValue(NavigationTypeProperty, value);
+    }
+}
