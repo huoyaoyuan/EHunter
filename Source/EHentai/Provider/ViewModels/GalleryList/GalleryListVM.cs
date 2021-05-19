f110c55a7af16be767468d4c9731ee97f71d7aab
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-05-20 01:06:01 +0800

    Show loaded front page galleries.

--- Source/EHentai/Provider/ViewModels/GalleryList/GalleryListVM.cs
+++ Source/EHentai/Provider/ViewModels/GalleryList/GalleryListVM.cs
@@ -1,6 +1,27 @@
-﻿namespace EHunter.EHentai.ViewModels.GalleryList
+﻿using System;
+using System.Collections.ObjectModel;
+using EHunter.EHentai.Api;
+using EHunter.EHentai.Api.Models;
+
+namespace EHunter.EHentai.ViewModels.GalleryList
 {
     public class GalleryListVM
     {
+        private readonly EHentaiClient _client;
+
+        public GalleryListVM(EHentaiClient client)
+        {
+            _client = client;
+            LoadAsync();
+
+            async void LoadAsync()
+            {
+                var page = await _client.GetPageAsync(new Uri("https://exhentai.org")).ConfigureAwait(true);
+                foreach (var g in page)
+                    Galleries.Add(g);
+            }
+        }
+
+        public ObservableCollection<Gallery> Galleries { get; } = new();
     }
 }
---
+++ Source/EHentai/UI/Views/GalleryList/GalleryListTab.xaml.cs
@@ -0,0 +1,25 @@
+﻿using EHunter.EHentai.ViewModels.GalleryList;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+#nullable enable
+
+namespace EHunter.EHentai.Views.GalleryList
+{
+    public sealed partial class GalleryListTab : UserControl
+    {
+        public GalleryListTab() => InitializeComponent();
+
+        public static readonly DependencyProperty VMProperty
+            = DependencyProperty.Register(nameof(VM), typeof(GalleryListVM), typeof(GalleryListTab),
+                new PropertyMetadata(null));
+        public GalleryListVM? VM
+        {
+            get => (GalleryListVM?)GetValue(VMProperty);
+            set => SetValue(VMProperty, value);
+        }
+    }
+}
