f28e8ea180ed15bbd2d54687a97073feb1ae2411
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-06-19 21:10:43 +0800

    Animated in image service.

--- Source/Pixiv/Provider/Services/ImageCaching/PixivImageService.cs
+++ Source/Pixiv/Provider/Services/ImageCaching/PixivImageService.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.Composition;
-using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
 using EHunter.Media;
 using Meowtrix.PixivApi.Models;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace EHunter.Pixiv.Services.ImageCaching
 {
-    [Export]
-    public class PixivImageService
+    [Export, Shared]
+    public partial class PixivImageService
     {
         private readonly IMemoryCache _memoryCache;
 
@@ -19,49 +16,11 @@ namespace EHunter.Pixiv.Services.ImageCaching
 
         public IImageSource GetImage(ImageInfo imageInfo) => new PixivImageSource(this, imageInfo);
 
-        private class PixivImageSource : IImageSource
+        public IImageSource GetAnimatedIllust(Illust illust)
         {
-            private readonly PixivImageService _owner;
-            private readonly ImageInfo _imageInfo;
-            private byte[]? _data;
-
-            public PixivImageSource(PixivImageService owner, ImageInfo imageInfo)
-            {
-                _owner = owner;
-                _imageInfo = imageInfo;
-            }
-
-            public ValueTask<Stream> GetImageAsync(bool refresh = false, CancellationToken cancellationToken = default)
-            {
-                cancellationToken.ThrowIfCancellationRequested();
-
-                if (!refresh && _data != null)
-                    return new(new MemoryStream(_data));
-
-                var cacheKey = new PixivCacheKey(_imageInfo.Uri);
-                if (!refresh)
-                {
-                    if (_owner._memoryCache.TryGetValue(cacheKey, out _data))
-                        return new(new MemoryStream(_data!));
-                }
-                else
-                {
-                    _data = null;
-                    _owner._memoryCache.Remove(cacheKey);
-                }
-
-                return GetAsync(cacheKey, cancellationToken);
-
-                async ValueTask<Stream> GetAsync(PixivCacheKey cacheKey, CancellationToken cancellationToken)
-                {
-                    var response = await _imageInfo.RequestAsync(cancellationToken).ConfigureAwait(false);
-                    _data = await response.Content.ReadAsByteArrayAsync(cancellationToken).ConfigureAwait(false);
-                    _owner._memoryCache.Set(cacheKey, _data);
-                    return new MemoryStream(_data);
-                }
-            }
+            if (!illust.IsAnimated)
+                throw new ArgumentException("Must be animated illust.", nameof(illust));
+            return new PixivAnimatedImageSource(this, illust);
         }
     }
-
-    internal record PixivCacheKey(Uri Uri);
 }
