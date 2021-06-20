47a321a9477af0ddd5a3aa7c5264679b7f5b9ac2
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-06-20 22:19:45 +0800

    Use CachedImageSource.

---
+++ Source/Common/Base/Services/ImageCaching/CachedImageSource.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using EHunter.Media;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace EHunter.Services.ImageCaching
+{
+    public abstract class CachedImageSource : IImageSource
+    {
+        private readonly IMemoryCache _memoryCache;
+        private byte[]? _data;
+
+        protected CachedImageSource(IMemoryCache memoryCache) => _memoryCache = memoryCache;
+
+        public async ValueTask<Stream> GetImageAsync(bool refresh = false, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (!refresh)
+            {
+                if (_data is not null)
+                    return new MemoryStream(_data);
+            }
+
+            object cacheKey = CreateCacheKey();
+            if (refresh)
+            {
+                _data = null;
+                _memoryCache.Remove(cacheKey);
+            }
+
+            _data = await _memoryCache.GetOrCreateAsync(cacheKey, async entry =>
+            {
+                var stream = await RequestAsync(cancellationToken).ConfigureAwait(false);
+                entry.SetSize(stream.Length);
+                byte[] data = new byte[stream.Length];
+                await stream.CopyToAsync(new MemoryStream(data)).ConfigureAwait(false);
+                return data;
+            }).ConfigureAwait(false);
+
+            return new MemoryStream(_data);
+        }
+
+        protected abstract object CreateCacheKey();
+
+        protected abstract Task<Stream> RequestAsync(CancellationToken cancellationToken = default);
+    }
+}
--- Source/Pixiv/Provider/Services/Images/PixivImageService.cs
+++ Source/Pixiv/Provider/Services/Images/PixivImageService.cs
@@ -1,14 +1,38 @@
 ﻿using System.Composition;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using EHunter.Media;
+using EHunter.Services.ImageCaching;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace EHunter.Pixiv.Services.Images
 {
     [Export, Shared]
-    public partial class PixivImageService
+    public class PixivImageService
     {
         private readonly IMemoryCache _memoryCache;
 
         [ImportingConstructor]
         public PixivImageService(IMemoryCache memoryCache) => _memoryCache = memoryCache;
+
+        public IImageSource GetImage(ImageRequest request) => new ImageSource(this, request);
+
+        private class ImageSource : CachedImageSource
+        {
+            private readonly PixivImageService _owner;
+            private readonly ImageRequest _request;
+
+            public ImageSource(PixivImageService owner, ImageRequest request)
+                : base(owner._memoryCache)
+            {
+                _owner = owner;
+                _request = request;
+            }
+
+            protected override object CreateCacheKey() => _request.GetCacheKey();
+            protected override Task<Stream> RequestAsync(CancellationToken cancellationToken = default)
+                => _request.RequestAsync(cancellationToken);
+        }
     }
 }
