8261fcd9ffc13e22e57a989e3c486e9f1a22c829
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-06-20 22:21:06 +0800

    Remove design of IStorageService.

--- Source/Common/Base/Services/ImageCaching/ImageCacheService.cs
+++ Source/Common/Base/Services/ImageCaching/ImageCacheService.cs
@@ -1,13 +1,11 @@
 ﻿using System.Composition;
-using System.IO;
 using System.Threading.Tasks;
-using EHunter.Media;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace EHunter.Services.ImageCaching
 {
     [Export, Shared]
-    public class ImageCacheService : IStorageService<ICachable>
+    public class ImageCacheService
     {
         private readonly IMemoryCache _memoryCache;
 
@@ -29,24 +27,6 @@ namespace EHunter.Services.ImageCaching
                 entry.SetSize(data.Length);
                 return new ImageEntry(data);
             }).ConfigureAwait(false);
-        }
-
-        public ValueTask<bool> IsStored(ICachable key) => new(_memoryCache.TryGetValue(key.GetCacheKey(), out _));
-
-        public ValueTask<Stream?> TryGet(ICachable key)
-        {
-            if (_memoryCache.TryGetValue(key.GetCacheKey(), out byte[] value))
-                return new(new MemoryStream(value));
-            return default;
-        }
-
-        public async ValueTask Store(ICachable key, Stream stream)
-        {
-            stream.Seek(0, SeekOrigin.Begin);
-            byte[] data = new byte[stream.Length];
-            await stream.CopyToAsync(new MemoryStream(data)).ConfigureAwait(false);
-            _memoryCache.Set(key.GetCacheKey(), data);
-            stream.Seek(0, SeekOrigin.Begin);
         }
     }
 }
