a9814d0ab56874f664b20e4dc9ca0a94553e3851
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-06-22 23:36:08 +0800

    Separated all download vm.

---
+++ Source/Pixiv/Provider/ViewModels/Download/AllDownloadsVM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Composition;
+using DynamicData;
+
+namespace EHunter.Pixiv.ViewModels.Download
+{
+    [Export, Shared]
+    public sealed class AllDownloadsVM : IDisposable
+    {
+        private readonly IDisposable _subscription;
+
+        private readonly ReadOnlyObservableCollection<ActiveDownloadVM> _tasks;
+        public ReadOnlyObservableCollection<ActiveDownloadVM> Tasks => _tasks;
+
+        [ImportingConstructor]
+        public AllDownloadsVM(DownloadManager downloadManager, PixivVMFactory factory)
+        {
+            _subscription = downloadManager.QueuedTasks
+                .Connect()
+                .Transform(x => new ActiveDownloadVM(x, factory))
+                .Bind(out _tasks)
+                .DisposeMany()
+                .Subscribe();
+        }
+
+        public void Dispose() => _subscription.Dispose();
+    }
+}
