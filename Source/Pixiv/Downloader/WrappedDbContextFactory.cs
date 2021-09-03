9eb825f926b69c8557a5edb40a061198526cf8d5
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-09-04 00:04:37 +0800

    Workaround for CA2252

--- Source/Pixiv/Downloader/WrappedDbContextFactory.cs
+++ Source/Pixiv/Downloader/WrappedDbContextFactory.cs
@@ -3,7 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EHunter.Pixiv.Downloader.Manual
 {
+#pragma warning disable CA2252 // TODO: https://github.com/dotnet/roslyn-analyzers/issues/5366
     internal class WrappedDbContextFactory<TContext> : IDbContextFactoryResolver<TContext>
+#pragma warning restore CA2252
         where TContext : DbContext
     {
         private readonly IDbContextFactory<TContext>? _service;
