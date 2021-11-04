9e5c7ab1773b53405d00b20910d1d4c05c190469
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-11-04 15:26:13 +0800

    Use markupextension to localize enum

---
+++ Source/Common/UI/Converters/EnumLocalizeConverter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.UI.Xaml.Data;
+using Microsoft.Windows.ApplicationModel.Resources;
+
+namespace EHunter.Converters
+{
+    public sealed class EnumLocalizeConverter : IValueConverter
+    {
+        private readonly ResourceManager _resourceManager = new();
+
+        public object? Convert(object? value, Type targetType, object? parameter, string language)
+        {
+            if (value is null)
+                return null;
+
+            var resourceMap = _resourceManager.MainResourceMap;
+            if (parameter is not null)
+                resourceMap = resourceMap.TryGetSubtree(parameter.ToString());
+
+            return resourceMap
+                ?.TryGetValue($"{value.GetType().Name}_{value}")
+                ?.ValueAsString;
+        }
+
+        public object ConvertBack(object? value, Type targetType, object? parameter, string language)
+            => throw new NotSupportedException();
+    }
+}
