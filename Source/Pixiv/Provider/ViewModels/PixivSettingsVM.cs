702f726273fb82b08121edd601be52e02f50ff28
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-11-07 02:52:05 +0800

    Add back user avatar in settings

--- Source/Pixiv/Provider/ViewModels/PixivSettingsVM.cs
+++ Source/Pixiv/Provider/ViewModels/PixivSettingsVM.cs
@@ -1,6 +1,8 @@
 ﻿using System.Composition;
+using System.Diagnostics;
 using CommunityToolkit.Mvvm.ComponentModel;
 using EHunter.DependencyInjection;
+using EHunter.Media;
 using EHunter.Pixiv.Settings;
 using Meowtrix.PixivApi;
 
@@ -12,16 +14,22 @@ namespace EHunter.Pixiv.ViewModels
         private readonly PixivSetting _settings;
 
         [ImportingConstructor]
-        public PixivSettingsVM(PixivSetting settings, ICustomResolver<PixivClient> clientResolver)
+        public PixivSettingsVM(PixivSetting settings,
+            ICustomResolver<PixivClient> clientResolver,
+            PixivVMFactory vmFactory)
         {
             _settings = settings;
 
             _maxDownloadsInParallel = _settings.MaxDownloadsInParallel.Value;
             _connectionMode = _settings.ConnectionMode.Value;
             Client = clientResolver.Resolve();
+            Debug.Assert(Client.CurrentUser != null);
+            MyAvatar = vmFactory.GetImage(Client.CurrentUser.Avatar);
         }
 
         public PixivClient Client { get; }
+
+        public IImageSource MyAvatar { get; }
 
         private int _maxDownloadsInParallel;
         public int MaxDownloadsInParallel
