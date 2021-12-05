88a3dc26660975a9a825a11ddfbdf1ac580ec168
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-12-06 02:07:01 +0800

    Parse image page

--- Source/EHentai/Provider/Api/Models/GalleryPage.cs
+++ Source/EHentai/Provider/Api/Models/GalleryPage.cs
@@ -1,4 +1,8 @@
-﻿namespace EHunter.EHentai.Api.Models
+﻿using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+
+namespace EHunter.EHentai.Api.Models
 {
     public class GalleryPage
     {
@@ -15,5 +19,18 @@
         public Uri Thumbnail { get; }
 
         public Task<Stream> RequestThumbnailAsync() => _client.HttpClient.GetStreamAsync(Thumbnail);
+
+        public async Task<Stream> GetImageAsync(CancellationToken cancellationToken = default)
+        {
+            var config = Configuration.Default;
+            var context = BrowsingContext.New(config);
+
+            using var request = await _client.HttpClient.GetStreamAsync(PageUri, cancellationToken).ConfigureAwait(false);
+            var document = await context.OpenAsync(req => req.Content(request), cancellationToken).ConfigureAwait(false);
+
+            var image = document
+                .QuerySelector<IHtmlImageElement>("img#img");
+            return await _client.HttpClient.GetStreamAsync(image!.Source, cancellationToken).ConfigureAwait(false);
+        }
     }
 }
