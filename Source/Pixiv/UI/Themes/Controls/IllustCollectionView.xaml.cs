f6962acca1b687afba5cac74f2f7b3f7f51ba1d4
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-12-08 23:13:35 +0800

    Add workaround for navigation issue

--- Source/Pixiv/UI/Themes/Controls/IllustCollectionView.xaml.cs
+++ Source/Pixiv/UI/Themes/Controls/IllustCollectionView.xaml.cs
@@ -1,7 +1,16 @@
-﻿namespace EHunter.Pixiv.Themes
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls.Primitives;
+
+namespace EHunter.Pixiv.Themes
 {
     internal partial class IllustCollectionViewTheme
     {
         public IllustCollectionViewTheme() => InitializeComponent();
+
+        private void HyperlinkButton_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO: workaround for https://github.com/microsoft/microsoft-ui-xaml/issues/6350
+            ((ButtonBase)sender).ReleasePointerCaptures();
+        }
     }
 }
