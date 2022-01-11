d1ec8ad2cd8bbee91093e95f7841680f38b867cc
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2022-01-11 21:04:15 +0800

    Remove global suppression file

--- Source/App/WinUI/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Security", "CA5392: Use DefaultDllImportSearchPaths attribute for P/Invoke", Justification = "Generated code", Scope = "member", Target = "~M:EHunter.UI.Program.XamlCheckProcessRequirements")]
