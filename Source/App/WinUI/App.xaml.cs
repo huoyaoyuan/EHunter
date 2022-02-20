2020830b38f3df9481fe3a75b201d10860e80416
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2022-02-20 21:56:56 +0800

    Move service configuration to separate class

--- Source/App/WinUI/App.xaml.cs
+++ Source/App/WinUI/App.xaml.cs
@@ -1,10 +1,5 @@
-﻿using CommunityToolkit.Mvvm.Messaging;
-using EHunter.EHentai;
+﻿using EHunter.EHentai;
 using EHunter.Pixiv;
-using EHunter.Providers;
-using EHunter.Services;
-using EHunter.Settings;
-using EHunter.UI.Services;
 using EHunter.UI.ViewModels;
 using EHunter.UI.Views;
 using Microsoft.Extensions.DependencyInjection;
@@ -26,36 +21,7 @@ namespace EHunter.UI
         /// </summary>
         public App()
         {
-            var providers = new IEHunterProvider[]
-            {
-                new PixivUIProvider(),
-                new EHentaiUIProvider(),
-            };
-
-            var services = new ServiceCollection()
-                .AddMemoryCache(o =>
-                {
-                    o.SizeLimit = 2 * (1L << 30);
-                    o.CompactionPercentage = 0.9;
-                })
-                .AddSingleton<IMessenger, WeakReferenceMessenger>()
-                .AddSingleton<IViewModelService, ViewModelService>()
-                .AddSingleton<ISettingsStore, WinRTSettingsStore>()
-                .AddSingleton<CommonSetting>()
-                .AddSingleton<IStorageSetting>(sp => sp.GetRequiredService<CommonSetting>())
-                .AddSingleton<IDatabaseSetting>(sp => sp.GetRequiredService<CommonSetting>())
-                .AddSingleton<IProxySetting>(sp => sp.GetRequiredService<CommonSetting>())
-                .AddSingleton<ICommonSettingStore, CommonSettingStore>()
-                .AddSingleton<MainWindowVM>()
-                .AddTransient<CommonSettingVM>();
-
-            foreach (var provider in providers)
-            {
-                services.AddSingleton(provider);
-                provider.ConfigureServices(services);
-            }
-
-            _serviceProvider = services.BuildServiceProvider();
+            _serviceProvider = EHunterApp.Build(new PixivUIProvider(), new EHentaiUIProvider());
 
             InitializeComponent();
         }
---
+++ Source/App/WinUI/EHunterApp.cs
@@ -0,0 +1,41 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using EHunter.Providers;
+using EHunter.Services;
+using EHunter.Settings;
+using EHunter.UI.Services;
+using EHunter.UI.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EHunter.UI
+{
+    public static class EHunterApp
+    {
+        public static IServiceProvider Build(params IEHunterProvider[] providers)
+        {
+            var services = new ServiceCollection()
+                .AddMemoryCache(o =>
+                {
+                    o.SizeLimit = 2 * (1L << 30);
+                    o.CompactionPercentage = 0.9;
+                })
+                .AddSingleton<IMessenger, WeakReferenceMessenger>()
+                .AddSingleton<IViewModelService, ViewModelService>()
+                .AddSingleton<ISettingsStore, WinRTSettingsStore>()
+                .AddSingleton<CommonSetting>()
+                .AddSingleton<IStorageSetting>(sp => sp.GetRequiredService<CommonSetting>())
+                .AddSingleton<IDatabaseSetting>(sp => sp.GetRequiredService<CommonSetting>())
+                .AddSingleton<IProxySetting>(sp => sp.GetRequiredService<CommonSetting>())
+                .AddSingleton<ICommonSettingStore, CommonSettingStore>()
+                .AddSingleton<MainWindowVM>()
+                .AddTransient<CommonSettingVM>();
+
+            foreach (var provider in providers)
+            {
+                services.AddSingleton(provider);
+                provider.ConfigureServices(services);
+            }
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
